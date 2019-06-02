3292e942ef864a7ab68b464e199feb14e8ee87fc
serhii benedyshyn <serhiib@MacBook-Pro-serhii.local>
2019-06-02 22:15:43 +0300

    Use Native UI Components

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -4,6 +4,7 @@ import {
   Text,
   View,
   Image,
+  TouchableOpacity,
   TouchableHighlight
 } from 'react-native';
 
@@ -11,50 +12,70 @@ export default class Toolbar extends Component {
 
   get computedSources() {
     return {
-      callButtonImage:        this.props.callButtonState        ? require('../images/callStart.png')    : require('../images/callEnd.png'),
-      cameraButtonImage:      this.props.cameraButtonState      ? require('../images/cameraOn.png')     : require('../images/cameraOff.png'),
-      microphoneButtonImage:  this.props.microphoneButtonState  ? require('../images/microphoneOn.png') : require('../images/microphoneOff.png')
+      callButtonImage:       this.props.callButtonState       ? require('../assets/callEnd.png')       : require('../assets/callStart.png'),
+      cameraButtonImage:     this.props.cameraButtonState     ? require('../assets/cameraOff.png')     : require('../assets/cameraOn.png'),
+      microphoneButtonImage: this.props.microphoneButtonState ? require('../assets/microphoneOff.png') : require('../assets/microphoneOn.png')
     }
   }
 
   render() {
-    if (this.props.hideToolbar) {
-      return null;
+    if (this.props.isToolbarHidden) {
+      return (
+        <View style={ styles.wrapper }>
+          <TouchableOpacity
+              style   = { styles.arrowHelperButton }
+              onPress = { this.props.togglePressHandler }>
+            <Image 
+              style  = { styles.arrowHelperImage }
+              source = { require('../assets/toolbar-arrow-up.png') }
+            />
+          </TouchableOpacity>
+        </View>
+      );
     } else {
       return (
-        <View style = { styles.toolbar }>
-          <View style = { styles.toolbarLeft } />
-          <View style = { styles.toolbarCenter }>
-            <TouchableHighlight 
-                id      = { 0 }
-                style   = { styles.toolbarButton } 
-                onPress = { this.props.cameraButtonPressHandler.bind(this) }>
-              <Image 
-                  style = { styles.buttonImage }
-                  source = { this.computedSources.cameraButtonImage }
-              />
-            </TouchableHighlight>
-            <TouchableHighlight 
-                style   = { styles.toolbarButton } 
-                onPress = { this.props.callButtonPressHandler.bind(this) } >
-              <Image 
-                  style   = { styles.buttonImage }
-                  source  = { this.computedSources.callButtonImage }
-              />
-            </TouchableHighlight>
-            <TouchableHighlight 
-                style   = { styles.toolbarButton } 
-                onPress = { this.props.microphoneButtonPressHandler.bind(this) }>
-              <Image 
-                  style   = { styles.buttonImage }
-                  source  = { this.computedSources.microphoneButtonImage }
-              />
-            </TouchableHighlight>
-          </View>
-          <View style = { styles.toolbarRight }>
-            <Text style = { styles.text }>
-              { this.props.clientVersion }
-            </Text>
+        <View style={ styles.wrapper }>
+          <TouchableOpacity
+              style   = { styles.arrowHelperButton }
+              onPress = { this.props.togglePressHandler }>
+            <Image 
+              style  = { styles.arrowHelperImage }
+              source = { require('../assets/toolbar-arrow-down.png') }
+            />
+          </TouchableOpacity>
+          <View style = { styles.toolbar }>
+            <View style = { styles.toolbarLeft } />
+            <View style = { styles.toolbarCenter }>
+              <TouchableHighlight 
+                  style   = { styles.toolbarButton } 
+                  onPress = { this.props.cameraButtonPressHandler }>
+                <Image 
+                    style  = { styles.buttonImage }
+                    source = { this.computedSources.cameraButtonImage }
+                />
+              </TouchableHighlight>
+              <TouchableHighlight 
+                  style   = { styles.toolbarButton } 
+                  onPress = { this.props.callButtonPressHandler }>
+                <Image 
+                    style   = { styles.buttonImage }
+                    source  = { this.computedSources.callButtonImage }
+                />
+              </TouchableHighlight>
+              <TouchableHighlight 
+                  style   = { styles.toolbarButton } 
+                  onPress = { this.props.microphoneButtonPressHandler }>
+                <Image 
+                    style   = { styles.buttonImage }
+                    source  = { this.computedSources.microphoneButtonImage }
+                />
+              </TouchableHighlight>
+            </View>
+            <View style = { styles.toolbarRight }>
+              <Text style = { styles.text }>
+                { this.props.clientVersion }
+              </Text>
+            </View>
           </View>
         </View>
       );
@@ -63,31 +84,33 @@ export default class Toolbar extends Component {
 }
 
 const styles = StyleSheet.create({
-  toolbar: {
-    flexDirection:    "row",
-    alignItems:       'flex-end',
-    height:            180
+  wrapper: {
+    flexDirection:     "column",
+    alignItems:        "center",
+    justifyContent:    "center",
+    width:             "100%"
   },
-  
+  toolbar: {
+    flexDirection:     "row",
+    alignItems:        "flex-end",
+    height:             50
+  },
   toolbarLeft: {
     paddingLeft:        10,
     width:             "30%"
   },
-
   toolbarCenter: {
     width:             "40%",
     flexDirection:     "row",
     alignItems:        "center",
     justifyContent:    "center"
   },
-
   toolbarRight: {
     paddingRight:        10,
     width:              "30%",
     flexDirection:      "row",
     justifyContent:     "flex-end",
   },
-
   toolbarButton: {
     backgroundColor:    "rgba(0, 0, 0, 0.25)",
     flexDirection:      "row",
@@ -99,15 +122,30 @@ const styles = StyleSheet.create({
     height:              50,
     borderRadius:        20
   },
-
   buttonImage: {
     width:               23,
     height:              23
   },
-
   text: {
     fontSize:            12,
     color:              "rgba(60, 60, 60, 0.25)",
     marginTop:          "85%"
+  },
+  arrowHelperButton: {
+    backgroundColor:    "rgba(0, 0, 0, 0.15)",
+    flexDirection:      "row",
+    alignItems:         "center",
+    justifyContent:     "center",
+    margin:              20,
+    width:               40,
+    height:              20,
+    borderRadius:        20
+  },
+  arrowHelperImage: {
+    width:               16,
+    height:              12,
+    opacity:             0.3,
+    marginTop:           20,
+    marginBottom:        20,
   }
 });
--- ios/VidyoReactNative/AppDelegate.h
+++ ios/VidyoReactNative/AppDelegate.h
@@ -1,13 +1,15 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
+#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-#import <React/RCTBridge.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-  @property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
 @end
